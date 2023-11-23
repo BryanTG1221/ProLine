b0398508920ad96171e3b5a84724263b94046f0c
BryanTurrubiates <bryanturrubiates@outlook.com>
2023-11-23 10:25:46 -0600

    feat: (Login) Added landingSide

--- src/auth/Login.tsx
+++ src/auth/Login.tsx
@@ -1,13 +1,15 @@
+import { type FormEvent } from "react";
+import LandingSide from '@assets/landingLogin.jpg'
 import { Input, Checkbox, Button } from "@nextui-org/react";
 import { FiArrowRight } from "react-icons/fi";
 import logo from '@assets/logo.svg'
+import logoWhiteZT from '@assets/ZTStudiosWhite.svg'
 import Styles from './styles/Login.module.css'
-import { type FormEvent } from "react";
 
 export function Login () {
   async function handleSubmit (event: FormEvent<HTMLFormElement>) {
     event.preventDefault()
-    console.log("Hola hiciste cick")
+    console.log("Hola hiciste cick")  
     const form = event.currentTarget
     const formData = new FormData(form)
     const email = formData.get('email') as string
@@ -19,17 +21,18 @@ export function Login () {
     })
     const response = await fetchingData.json()
     console.log(response)
+    
   }
   return (
-    <main>
+    <main className={Styles.mainContainer}>
       <section className={Styles.container}>
         <form className={Styles.containerForm} onSubmit={handleSubmit}>
           <section style={{display: 'flex', justifyContent: 'center', paddingBottom: '20px'}}>
             <img src={logo} width={150}/>
           </section>
           <div style={{display: 'flex', flexDirection:'column', gap: '4px', lineHeight: '20px'}}>
-            <p className={Styles.title}>Bienvenido de vuelta!</p>
-            <p className={Styles.subTitle}>Iniciar Sesion</p>
+            <p className={Styles.title}>Welcome back!</p>
+            <p className={Styles.subTitle}>Login</p>
           </div>
           <div style={{display: 'flex', flexDirection: 'column',gap: '12px'}}>
             <section>
@@ -40,10 +43,26 @@ export function Login () {
               <label className={Styles.labelText}>Password</label>
               <Input name="password" variant="bordered" radius="sm" key='email' type="password" labelPlacement="outside" placeholder="Enter you password"  />
             </section>
-            <Checkbox classNames={{ icon: Styles.icon, label: Styles.labelCheck }} style={{fontSize: '10px'}} color="warning">Recuerdame</Checkbox>
+            <Checkbox classNames={{ icon: Styles.icon, label: Styles.labelCheck }} style={{fontSize: '10px'}} color="warning">Remind me</Checkbox>
           </div>
           <Button type="submit" style={{ backgroundColor: 'var(--primaryColor)', color: 'white', fontSize: '1rem', borderRadius: '8px'}} endContent={<FiArrowRight style={{fontSize: '1.2rem'}} />}>Iniciar Sesion</Button>
         </form>
+      </section>
+      <section className={Styles.landingSide}>
+        <div className={Styles.contentImage}>
+          <section className={Styles.imageSection}>
+            <div className={Styles.filterImg} />
+            <img src={LandingSide} className={Styles.image} />
+          </section>
+          <section className={Styles.infoSection}>
+            <p className={Styles.infoTitle}>Who we are?</p>
+            <p className={Styles.description}>In Proline, explore a vast array of vehicles, from luxurious motorcycles to dream cars. Our mission is to provide you with the utmost ease in acquiring the vehicle of your dreams.</p>
+          </section>
+          <div className={Styles.poweredBy}>
+            <p>Powered by</p>
+            <img src={logoWhiteZT}  />
+          </div>
+        </div>
       </section>
     </main>
   )
